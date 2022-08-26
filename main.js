773039b701be98a9e28990c2806d6a7dc9238d3d
kankshat <52341392+kankshat@users.noreply.github.com>
2022-08-26 16:13:06 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+var words = ["Enthusiastic.", "Developer.", "Passionate."];
+var i = 0;
+
+function wordCycler() {
+    setInterval(function () {
+        var word = words[i];
+        $("#random-word").fadeOut('slow', function () {
+            $(this).html(word).fadeIn("slow");
+        });
+        i = (i+1)%3
+    }, 3000);
+}
+
+wordCycler();
